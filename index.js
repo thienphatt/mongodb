3c166e5a26ecdd2bd5f3d9ab921a1245f9603188
thienphatt <buithienphat.btp@gmail.com>
2024-04-11 18:42:49 +0700

    add

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./db");
-const PORT = process.env.PORT || 8080;
+const PORT = 5000;
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const pingRoutes = require("./routes/pingRoutes");
-const mongoose = require("mongoose");
 const User = require("./models/User");
 app.use(express.json());
 
@@ -15,7 +14,14 @@ app.get("/", (req, res) => {
 });
 
 // test
-app.use("/ping", pingRoutes);
+app.use("/ping", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // /products
 app.use("/products", productRoutes);
